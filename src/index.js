aa98be3461fe03aaebb5f9854f824febf3504212
Andy VanEe <1andyvanee@gmail.com>
2014-01-20 01:43:43 -0700

    Add Goudy Bookletter to main page

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ var typesource = require('./typesource');
 
 var view = {
   title: "TypeSource",
-  samples: ['League Gothic and Gentium', 'Chunkfive and Fanwood-text'],
+  samples: [
+    'League Gothic and Gentium',
+    'Chunkfive and Fanwood-text',
+    'Goudy Bookletter 1911'
+  ],
   sampleArray: function(){
     return JSON.stringify(view.samples);
   }
